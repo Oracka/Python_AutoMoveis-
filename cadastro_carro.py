695a216609c1221509fe3832098621e9d0266428
Oracka <YuriTomaz2007@gmail.com>
2026-03-27 07:56:40 -0300

    adicao de algumas partes do cadastro_carro por agora

--- cadastro_carro.py
+++ cadastro_carro.py
@@ -1,8 +1,8 @@
-import leitura_banco
+import leitura_banco # importando outro doc é possível utilizar suas  funções e classes
 
 def cadastro_pecas() -> None: 
 
-
+    dados = leitura_banco.leitura_banco()
 
 
     print("-- cadastro Aqui --")
@@ -12,17 +12,27 @@ def cadastro_pecas() -> None:
     fabricante = input("fabricante: ")
     data = input("data de produção ")
     veiculo = input("veiculos compátiveis (separar por virgula) ")
+    
+    lista_veiculos = []
 
     novaPeca = {
-        "id": novo_id, 
+        "id": novo_id, # criar um id ba que adiciona +1 por cada nova peca para o banco
         "peca": peca,
         "tipo": tipo,
         "parte": parte,
-        "veiculos": lista_veiculos,
+        "veiculo": lista_veiculos, #  veiculos listados compativeis com a novaPeca
         "fabricante": fabricante,
         "data_fabricacao": data
     }
 
     peca.append(novaPeca)
 
+    dados["peca"] = peca
+
+    print(f"\na {peca} foi cadastrada || id referente: {}")
+
+    leitura_banco.salvar_banco(dados)
+
     cadastro_pecas()
+
+    
