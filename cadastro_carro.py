8a48caf3e59fa9c0323da248f7733d98436892dc
Oracka <YuriTomaz2007@gmail.com>
2026-04-03 18:11:40 -0300

    apenas mandando por agora pra atualizar com as modificações de voces.
    meu intuito e terminar o cadastro_carro com a
    parte da pesquisa

--- cadastro_carro.py
+++ cadastro_carro.py
@@ -11,59 +11,53 @@ def clearConsole():
 
 # Eu comentei esse blocão de código porque não sei como ele será usado...
 # Vocês podem usar ele se precisarem
-"""
-def cadastro_pecas() -> None: 
 
-
-
+def adicionar_pecas() -> None: # adicionar "pecas" ao bamco "dados", ele vai adicionando e aceita copias
+    
     dados = leitura_banco.leitura_banco()
 
+    print("'''Campo de adição de peca \n'''")
+
+    peca_nome = input("Nome da peça: ").strip().title() # Sempre vai acionar uma Maiuscula no inicio de uma palavra(filtrar erros do usuario)
+
+while True:
+    tipo = input("tipo da peça: \n [1]-Mecanica | [2]-Eletronica | [3|-Interna | [4]-Externa | [5]-iluminação [ainda nao existente]-Cancelar Registro") # impedir que o usuario coloque algo alem de int no input e aceitar entre as duas opções 
+    try:
+        if tipo == "1":
+            tipo = "Mecanica"
+            break
+        elif tipo == "2":
+            tipo = "Eletronica"
+            break
+        elif tipo == "3":
+            tipo = "Interna"
+            break
+        elif tipo == "4":
+            tipo = "Externa"
+            break 
+        elif tipo == "5":
+            tipo = "Iluminação"
+        # ! adicionar depois uma alternativa de saida para o menu principal(main)
+        else:
+            print("! Coloque uma opção viavel entre as 1 e 4")
+    except ValueError:
+        print("coloque um valor inteiro para escolher entre as opções | EX( ... : 1 (vai aparecer 'mecanico' na saida))")
+
+print(f"tipo de peça escolhida: {tipo} \n continuando...")
+
+if tipo == "Mecanica":
+     
 
 
-    
-    print("-- cadastro Aqui --")
-    peca = input("nome peça: ")
-    tipo = input("tipo: ")
-    parte = input("parte: ")
-    fabricante = input("fabricante: ")
-    data = input("data de produção ")
-    veiculo = input("veiculos compátiveis (separar por virgula) ")
-    
-    lista_veiculos = []
-
-    novaPeca = {
-    
-        # "id": novo_id, 
-        "peca": peca,   
-        "tipo": tipo,
-        "parte": parte,
-        # "veiculos": lista_veiculos,
-        
-        "id": novo_id, # criar um id ba que adiciona +1 por cada nova peca para o banco
-        "peca": peca,
-        "tipo": tipo,
-        "parte": parte,
-        "veiculo": lista_veiculos, #  veiculos listados compativeis com a novaPeca
-        
-        "fabricante": fabricante,
-        "data_fabricacao": data
-        
-    dados["peca"] = peca
-
-    print(f"\na {peca} foi cadastrada || id referente: {}")
-
-    leitura_banco.salvar_banco(dados)
-
-    cadastro_pecas()
-    }
-
-    peca.append(novaPeca)
 
 
-cadastro_pecas()
-"""
 
-clearConsole()
+
+
+def pesquisa_pecas():
+
+
+    clearConsole()
 
 while True:
     print("-----BD AUTOMOVEIS----")
